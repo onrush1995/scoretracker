f2715c4d4f53312f4cf888efc1c400714522e594
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-05 16:14:53 +0300

    Added error page to the route

---
+++ src/pages/Error.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
+
+const Error = () => {
+    return (
+        <Wrapper className='full-page'>
+            <div>Random</div>
+        </Wrapper>
+    );
+};
+
+export default Error;
