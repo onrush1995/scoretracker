135c0aeef03f7c8974a85b97132b6cd05f979cee
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-11 08:56:42 +0300

    Added error page in the application

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,7 +5,12 @@ import img from '../assets/images/not-found.svg';
 const Error = () => {
     return (
         <Wrapper className='full-page'>
-            <div>Random</div>
+            <div>
+                <img src={img} alt='Not found' />
+                <h3>page not found</h3>
+                <p>It seems the pages you are looking does not exist</p>
+                <Link to='/'>Back Home</Link>
+            </div>
         </Wrapper>
     );
 };
