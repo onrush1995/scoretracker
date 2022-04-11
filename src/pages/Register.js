a25813e563aaa74955cf930f3a38868743736798
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-11 10:55:20 +0300

    Completed initial register component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,47 @@
-const Register = () => {
-    return <h1>Register</h1>;
+import { useState, useEffect } from 'react';
+import Logo from '../components/Logo';
+import FormRow from '../components/FormRow';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true
 };
 
+const Register = () => {
+    const [
+        values,
+        setValues
+    ] = useState(initialState);
+    const handleChange = (e) => {
+        console.log(e.target);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+    };
+
+    return (
+        <Wrapper className='full-page'>
+            <form className='form' onSubmit={onSubmit}>
+                <Logo />
+                <h3>Login</h3>
+                {/* name input for the form */}
+                <FormRow type='text' name='name' value={values.name} handleChange={handleChange} />
+
+                {/* email input for the form  */}
+                <FormRow type='email' name='email' value={values.email} handleChange={handleChange} />
+
+                {/* password for the form  */}
+                <FormRow type='password' name='password' value={values.password} handleChange={handleChange} />
+                <button type='sumbit' className='btn btn-block'>
+                    Submit
+                </button>
+            </form>
+        </Wrapper>
+    );
+};
 export default Register;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Dashboard from './Dashboard';
 import Register from './Register';
 import Landing from './Landing';
 import Error from './Error';
-
-export { Dashboard, Register, Landing, Error };
+import Logo from '../components/Logo';
+import FormRow from '../components/FormRow';
+export { FormRow, Logo, Dashboard, Register, Landing, Error };
