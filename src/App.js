dd2573ecfdfff671a00bf323608d93951013576f
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-02 18:41:11 +0300

    Modified landing pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Dashboard, Register, Landing, Error } from './pages';
+import { Register, Landing, Error } from './pages';
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Dashboard />} />
+                <Route path='/' element={<Landing />} />
+                {/* <Route path='/' element={<Dashboard />} /> */}
                 <Route path='/register' element={<Register />} />
-                <Route path='/landing' element={<Landing />} />
+
                 <Route path='*' element={<Error />} />
             </Routes>
         </BrowserRouter>
