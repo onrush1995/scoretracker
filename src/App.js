156c64a5dbf535e0aaf88565d8ea9b53b5200e89
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-02 18:18:03 +0300

    Issues404: Solved for Netlify deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Dashboard, Register, Landing, Error } from './pages';
 function App() {
     return (
